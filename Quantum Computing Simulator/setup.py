458395a21efd9997798b4379f96e47011ba08fa3
SamuelMMT <taubenbergersam@gmail.com>
2024-01-23 18:08:47 +0100

    setup.py

---
+++ Quantum Computing Simulator/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quantum_computing_simulator',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    install_requires=[
+        'numpy',
+        'matplotlib'
+    ],
+    tests_require=[
+        'pytest'
+        # add other test dependencies here
+    ],
+)
